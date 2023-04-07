df14a92f3dc497367854c2af7b0560d3212c77fe
ShukranIsaac <imwakabira@ymail.com>
2023-04-07 12:22:27 +0200

    mysql database setup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const dbConnection = require('./src/utils/mysql.connector')
+
 const post = require('./src/posts/post.model')
 
 app.get('/api/v1', function(req, res) {
@@ -13,4 +15,10 @@ app.get('/api/v1/posts', function(req, res) {
 
 app.listen(3000, function() {
     console.log('EGENCO listening on port 3000')
+
+    dbConnection.connect(function(err) {
+        if (err) throw err
+
+        console.log("Connected to MySQL")
+    })
 })
