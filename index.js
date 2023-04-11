b1d892063fc320a903440b9b6e5a9c36fa9e88e0
ShukranIsaac <imwakabira@ymail.com>
2023-04-11 16:11:14 +0200

    add comments to post endpoint & method

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const dbConnection = require('./src/utils/mysql.connector')
 
 const { Post } = require('./src/posts/post.model')
-const { query } = require('express')
 
 // app.get('/api/v1', function (req, res) {
 //     return res.json(req.headers)
@@ -24,13 +23,17 @@ app.get('/api/v1/posts', function (req, res) {
     })
 })
 
+// Create new post/article in the database
 app.post('/api/v1/posts', function (req, res) {
     // console.log(req.body)
-    const { name, imageUrl, summary } = req.body
+    const { name, imageUrl, summary } = req.body // destructure sent properties from the REQUEST body
+
+    // construct sql query
     var sql = `INSERT INTO posts (name, imageUrl, summary) VALUES ('${name}','${imageUrl}','${summary}')`;
     // console.log(sql)
+    // Query the MySQL database and return result to the client app ie. POSTMAN or Web APP
     return dbConnection.query(sql, function (err, result) {
-        if (err) throw err;
+        if (err) throw err; // if error throw it, else continue execution to next line
         return res.json(result)
         // console.log("1 record inserted");
     });
