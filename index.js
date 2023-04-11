61394e07adf238839de233055e506f3555228145
ShukranIsaac <imwakabira@ymail.com>
2023-04-11 17:35:57 +0200

    docs: body parser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const bodyParser = require('body-parser')
-
+// Request Body parsing Middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
