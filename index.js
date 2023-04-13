2dbac6d3095d8c22735f8385c039a8ffc69038d1
ShukranIsaac <imwakabira@ymail.com>
2023-04-13 18:55:08 +0200

    update a single post given its id

--- index.js
+++ index.js
@@ -10,9 +10,45 @@ const dbConnection = require('./src/utils/mysql.connector')
 
 const { Post } = require('./src/posts/post.model')
 
-// app.get('/api/v1', function (req, res) {
-//     return res.json(req.headers)
-// })
+// Update existing post
+app.patch('/api/v1/posts/:id', function (request, response) {
+    // console.log(request.params)
+    // get id from request, use id to select a post from db, update post and end request
+    const sql = `SELECT * FROM posts WHERE id=${request.params.id} LIMIT 1`
+
+    return dbConnection.query(sql, function (err, rows) {
+        if (err) throw err
+
+        const post = request.body
+        // console.log(Object.entries(post))
+
+        if (rows.length >= 1) {
+            let props = ''
+        
+            props = Object.keys(post).map((key, index) => {
+                // console.log(key)
+                // console.log(Object.values(post).at(index))
+                return props += `${[key]}='${Object.values(post).at(index)}',`
+            })
+            // console.log(props[props.length - 1].slice(0, -1))
+
+            const updateSql = `UPDATE posts SET ${props[props.length - 1].slice(0, -1)} WHERE id=${rows[0].id}`
+
+            // console.log(updateSql)
+            return dbConnection.query(updateSql, function (err, result) {
+                return response.json(rows[0])
+            })
+        } else {
+            return response.status(404).json({
+                status: false,
+                statusCode: 404,
+                message: `Post with id ${request.params.id} does not exist.`
+            })
+        }
+    })
+})
+
+// Delete existing post
 
 app.get('/api/v1/posts', function (req, res) {
     var sql2 = "SElECT * FROM posts"
