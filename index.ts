c315eb132b04ef42f15c924a40b33f0e9a1c524d
ShukranIsaac <imwakabira@ymail.com>
2023-04-20 09:04:39 +0200

    configure an typeorm

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
-const express = require('express')
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import { AppDataSource } from './src/utils/mysql.connector'
+
 const app = express()
 
-const bodyParser = require('body-parser')
 // Request Body parsing Middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const dbConnection = require('./src/utils/mysql.connector')
+// const dbConnection = require('./src/utils/mysql.connector')
 
 // const { Post } = require('./src/posts/post.model')
 const posts = require('./src/posts/post.router')
@@ -17,9 +19,10 @@ app.use('/', posts)
 app.listen(3000, function () {
     console.log('EGENCO listening on port 3000')
 
-    dbConnection.connect(function (err) {
-        if (err) throw err
-
-        console.log("Connected to MySQL")
-    })
+    AppDataSource.initialize()
+        .then(conn => {
+            console.log("Connected to MySQL")
+        }).catch(err => {
+            if (err) throw err
+        })
 })
